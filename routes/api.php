88a5a42a390631a052a38797f374f9d0ab53bf88
Whitnu12 <whitnu@gmail.com>
2023-05-13 18:46:42 +0700

    Project icp for api 50%, only API focus rn

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GuruController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
@@ -25,6 +26,15 @@ Route::prefix('guru')->group(function () {
     Route::post('/', [GuruController::class, 'store']);
     Route::put('/{id}', [GuruController::class, 'update']);
     Route::delete('/{id}', [GuruController::class, 'destroy']);
+});
+
+
+Route::post('auth/register',[AuthController::class,'register']);
+Route::post('auth/login', [AuthController::class,'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('auth/profile', [AuthController::class, 'profile']);
+    Route::post('auth/logout', [AuthController::class, 'logout']);
 });
 
 
